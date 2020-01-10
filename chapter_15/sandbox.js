a14e967adc5972e78dd9b4af63bc5a6caeb8298e
Brandon Craig <bcraig2612@gmail.com>
2020-01-10 10:05:21 -0700

    Set Up Section 15 Folder

---
+++ chapter_15/sandbox.js
@@ -0,0 +1,25 @@
+// *** *** *** *** *** *** Section 15: Object Oriented JavaScript *** *** *** *** *** ***
+
+// *** *** *** *** *** *** WHAT IS OOP? *** *** *** *** *** ***
+
+// *** *** *** *** *** *** OBJECT LITERAL RECAP *** *** *** *** *** ***
+
+// *** *** *** *** *** *** CLASSES *** *** *** *** *** ***
+
+// *** *** *** *** *** *** CLASS CONSTRUCTORS *** *** *** *** *** ***
+
+// *** *** *** *** *** *** CLASS METHODS & METHOD CHAINING *** *** *** *** *** ***
+
+// *** *** *** *** *** *** CLASS INHERITANCE (SUBCLASSES) *** *** *** *** *** ***
+
+// *** *** *** *** *** *** SUPER() *** *** *** *** *** ***
+
+// *** *** *** *** *** *** CONSTRUCTORS (UNDER THE HOOD) *** *** *** *** *** ***
+
+// *** *** *** *** *** *** PROTOTYPE MODEL *** *** *** *** *** ***
+
+// *** *** *** *** *** *** PROTOTYPAL INHERITANCE *** *** *** *** *** ***
+
+// *** *** *** *** *** *** BUILT-IN OBJECTS *** *** *** *** *** ***
+
+// *** *** *** *** *** *** MAKING A FORECAST CLASS (WEATHER APP) *** *** *** *** *** ***
