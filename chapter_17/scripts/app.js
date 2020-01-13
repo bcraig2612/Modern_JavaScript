00d0661441f090f35bd475b5d83dc1ba4c229328
Brandon Craig <bcraig2612@gmail.com>
2020-01-12 17:14:22 -0700

    Finished Section-17

--- chapter_17/scripts/app.js
+++ chapter_17/scripts/app.js
@@ -1,1 +1,50 @@
 // *** *** *** *** *** *** Section-17: Real-time Chatroom Project *** *** *** *** *** ***
+
+// DOM queries
+const chatList = document.querySelector(".chat-list");
+const newChatForm = document.querySelector(".new-chat");
+const newNameForm = document.querySelector(".new-name");
+const updateMessage = document.querySelector(".update-mssg");
+const rooms = document.querySelector(".chat-rooms");
+
+// Add A New Chat
+newChatForm.addEventListener("submit", event => {
+  event.preventDefault();
+  const message = newChatForm.message.value.trim();
+  chatroom
+    .addChat(message)
+    .then(() => newChatForm.reset)
+    .catch(error => console.log(error));
+});
+
+// Update Username
+newNameForm.addEventListener("submit", event => {
+  event.preventDefault();
+  // Update Name Via Chatroom
+  const newName = newNameForm.name.value.trim();
+  chatroom.updateName(newName);
+  // Reset The Form
+  newNameForm.reset();
+  // Show Then Hide The Update Message
+  updateMessage.innerHTML = `Your name was updated to ${newName}`;
+  setTimeout(() => (updateMessage.innerText = ""), 3000);
+});
+
+//Update The Chatroom
+rooms.addEventListener("click", event => {
+  if (event.target.tagName === "BUTTON") {
+    chatUI.clear();
+    chatroom.updateRoom(event.target.getAttribute("id"));
+    chatroom.getChats(chat => chatUI.render(chat));
+  }
+});
+
+// Check Local Storage For A Username
+const username = localStorage.username ? localStorage.username : "anonymous";
+
+// Class Instances
+const chatUI = new ChatUI(chatList);
+const chatroom = new Chatroom("gaming", username);
+
+// Get Chats & Render
+chatroom.getChats(data => chatUI.render(data));
--- chapter_17/scripts/chat.js
+++ chapter_17/scripts/chat.js
@@ -1,1 +1,58 @@
 // *** *** *** *** *** *** Section-17: Real-time Chatroom Project *** *** *** *** *** ***
+// Adding New Chat Documents
+// Setting up a real-time listener to get new chats
+// Updating the username
+// Updating the room
+class Chatroom {
+  constructor(room, username) {
+    this.room = room;
+    this.username = username;
+    this.chats = db.collection("chats");
+    this.unsub;
+  }
+  async addChat(message) {
+    // format a chat object
+    const now = new Date();
+    const chat = {
+      message,
+      username: this.username,
+      room: this.room,
+      created_at: firebase.firestore.Timestamp.fromDate(now)
+    };
+    // save that chat document
+    const response = await this.chats.add(chat);
+    return response;
+  }
+  getChats(callback) {
+    this.unsub = this.chats
+      .where("room", "==", this.room)
+      .orderBy("created_at")
+      .onSnapshot(snapshot => {
+        snapshot.docChanges().forEach(change => {
+          if (change.type === "added") {
+            // update the UI
+            callback(change.doc.data());
+          }
+        });
+      });
+  }
+  updateName(username) {
+    this.username = username;
+    localStorage.setItem("username", username);
+  }
+  updateRoom(room) {
+    this.room = room;
+    console.log("THE CHAT ROOM HAS BEEN UPDATED");
+    if (this.unsub) {
+      this.unsub();
+    }
+  }
+}
+// setTimeout(() => {
+//   chatroom.updateRoom("gaming");
+//   chatroom.updateName("bcraig2612");
+//   chatroom.getChats(data => {
+//     console.log(data);
+//   });
+//   chatroom.addChat("Hola!");
+// }, 3000);
