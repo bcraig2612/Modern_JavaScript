1807fcaa63a7884f4aeb9f8bded5adf9ee4fd1d6
Brandon Craig <bcraig2612@gmail.com>
2020-01-13 10:48:07 -0700

    Completed Section 20 and the Course!

--- chapter_20/src/index.js
+++ chapter_20/src/index.js
@@ -1,1 +1,30 @@
+import Tooltip from "./ninja-ui/tooltip";
+import Dropdown from "./ninja-ui/dropdown";
+import Tabs from "./ninja-ui/tabs";
+import Snackbar from "./ninja-ui/snackbar";
 
+// create a tooltip
+const tooltip = new Tooltip(document.querySelector(".tooltip"));
+
+tooltip.init();
+
+// create 2 drop downs
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach(dropdown => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
+
+// create tabbed content
+const tabs = new Tabs(document.querySelector(".tabs"));
+tabs.init();
+
+// create snackbars
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  snackbar.show("you clicked me :)");
+});
